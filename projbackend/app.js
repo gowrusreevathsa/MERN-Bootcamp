79be82db7c423b73f9177e4922467029d071650c
Sreevathsa <sreevathsagowru@gmail.com>
2021-03-01 01:26:52 +0530

    env

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,18 +1,24 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const express = require("express");
 
 const app = express();
 
 mongoose
-  .connect(
-    "mongodb+srv://publicUser:publicPassword@cluster0.pfwtx.mongodb.net/ecommerce?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-  )
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
   .then(() => {
     console.log("DB CONNECTED");
+  })
+  .catch(() => {
+    console.log("OOPS! DB NOT CONNECTED");
   });
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`App is running on port: ${port}`);
